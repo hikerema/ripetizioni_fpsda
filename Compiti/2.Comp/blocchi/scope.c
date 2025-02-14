8a3ce34a84de9955d15c71671f8fe59bb126f0d6
Emanuele Moro <146356995+hikerema@users.noreply.github.com>
2025-02-14 15:39:25 +0100

    Aggiutni file

---
+++ Compiti/2.Comp/blocchi/scope.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void somma(int a, int b) { 
+    int risultato = a + b; 
+    printf("Somma: %d\n", risultato);
+}
+
+int main() {
+    somma(5, 7);
+    printf("%d", a);
+    return 0;
+}
